4a9e2bd1fbcfac05a9bb9742ebdbd21a5b961842
Jayam Vimal <crazylavendar@gmail.com>
2021-05-12 16:57:16 +0530

    Section 3 - Authentication+FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify"; //refer docs
+import "react-toastify/dist/ReactToastify.css";
+
+import Header from "./component/nav/Header";
+import Home from "./pages/Home";
+
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
-import Header from "./component/nav/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
+// import firebase from "firebase/app";
+// import "firebase/auth";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    //cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
-      <Header>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Switch>
-      </Header>
+      <Header />
+      <ToastContainer />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
+        <Route exact path="/forgot/password" component={ForgotPassword} />
+      </Switch>
     </>
   );
 };
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { auth } from "../../firebase";
+import { toast } from "react-toastify"; //refer docs
+import { useSelector } from "react-redux";
 
-const Register = () => {
+const Register = ({ history }) => {
+  const [email, setEmail] = useState("");
+
+  const { user } = useSelector((state) => {
+    return { ...state };
+  });
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]); // user as dependency
+  const config = {
+    url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+    handleCodeInApp: true,
+  };
+  const handleSubmit = async (e) => {
+    //
+    e.preventDefault(); // preventing browser from reload
+    // console.log("Env --> ", process.env.REACT_APP_REGISTER_REDIRECT_URL);
+
+    await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registration`
+    );
+
+    //save user email in local storage - Redoing again without asking email again
+
+    window.localStorage.setItem("emailForRegistration", email);
+
+    //clear state
+
+    setEmail("");
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        placeholder="Your Email"
+        // onChange={(e) => console.log(e.target.value)} // for debugging
+        onChange={(e) => setEmail(e.target.value)} // change state
+        autoFocus
+      />
+      <br />
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
     </div>
   );
 };
